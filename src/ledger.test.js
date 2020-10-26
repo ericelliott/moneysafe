fbb0f976e0ae1effd7cd22a75821fee80f2baba7
Alcides Bezerra <alcides.bsilvaneto@gmail.com>
2020-10-26 15:11:50 -0300

    Add test

--- src/ledger.test.js
+++ src/ledger.test.js
@@ -24,22 +24,41 @@ describe('$$', async assert => {
 });
 
 describe('addPercent(percent)', async assert => {
-  const should = 'add percents to the total';
+  {
+    const should = 'add percents to the total';
 
-  /* eslint-disable */
-  const actual = $$(
-    $(50),
-    addPercent(50)
-  ).toString();
-  /* eslint-enable */
-  const expected = $(75).toString();
+    /* eslint-disable */
+    const actual = $$(
+      $(50),
+      addPercent(50)
+    ).toString();
+    /* eslint-enable */
+    const expected = $(75).toString();
 
-  assert({
-    given: 'add percent function',
-    should,
-    actual,
-    expected
-  });
+    assert({
+      given: 'add percent function',
+      should,
+      actual,
+      expected
+    });
+  }
+
+  {    
+    /* eslint-disable */
+    const actual = $$(
+      $(100),
+      addPercent(50.29)
+    ).toString();
+    /* eslint-enable */
+    const expected = $(150.29).toString();
+    
+    assert({
+      given: 'add percent function (with float percentage number)',
+      should: "add percents to total",
+      actual,
+      expected
+    });
+  }
 });
 
 describe('subtract(percent)', async assert => {
